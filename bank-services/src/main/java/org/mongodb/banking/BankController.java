78eb8fcf237efb427526c48aff1f404eb3cc7112
Tom Wheeler <tom@temporal.io>
2024-12-11 21:11:58 -0600

    updated to use Javalin for API server

--- bank-services/src/main/java/org/mongodb/banking/BankController.java
+++ bank-services/src/main/java/org/mongodb/banking/BankController.java
@@ -1,164 +1,214 @@
 package org.mongodb.banking;
 
-import com.sun.net.httpserver.HttpServer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
 import java.util.List;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BankController {
+    
+    private static final Logger logger = LoggerFactory.getLogger(BankController.class);
+    
+    private static final String SUCCESS = "SUCCESS"; // service returns this to indicate success
+    private static final String ERROR = "ERROR";     // service returns this to indicate an error
+
     private final BankManager manager;
-    private final HttpServer server;
+    private final int port;
+    private final Javalin server;
 
     public BankController(BankManager manager, int port) throws IOException {
+        logger.info("Creating new BankController instance, with port=" + port);
+
         this.manager = manager;
-        this.server = HttpServer.create(new InetSocketAddress(port), 0);
-        initializeHandlers();
+        this.port = port;
+        
+        server = Javalin.create();
+        server.get("/api/balance", new GetBalanceHandler());
+        server.get("/api/createBank", new CreateBankHandler());
+        server.get("/api/deleteBank", new DeleteBankHandler());
+        server.get("/api/deposit", new DepositHandler());
+        server.get("/api/withdraw", new WithdrawHandler());
+        server.get("/api/isAvailable", new GetAvailabilityHandler());
+        server.get("/api/setAvailable", new SetAvailabilityHandler());
+        server.get("/api/listBanks", new ListBanksHandler());
+    }
+    
+    public void start() {
+        logger.info("Starting BankController on port " + port);
+        server.start("localhost", port);
     }
 
-    private void initializeHandlers() {
-        // Withdraw Endpoint
-        server.createContext("/api/withdraw", exchange -> {
+    private class GetBalanceHandler implements Handler {
+        @Override
+        public void handle(Context ctx) throws Exception {
+            logger.debug("BankController handling 'balance' request");
+
+            String bankName = ctx.req().getParameter("bankName");
+
             try {
-                RequestDetails request = parseRequest(exchange);
-                if (request == null) return;
-
-                String transactionId = request.service.withdraw(request.bankName, request.amount, request.idempotencyKey);
-                sendResponse(exchange, Map.of("status", "SUCCESS", "transactionId", transactionId));
-            } catch (Exception e) {
-                sendError(exchange, e.getMessage());
-            }
-        });
-
-        // Deposit Endpoint
-        server.createContext("/api/deposit", exchange -> {
-            try {
-                RequestDetails request = parseRequest(exchange);
-                if (request == null) return;
-
-                String transactionId = request.service.deposit(request.bankName, request.amount, request.idempotencyKey);
-                sendResponse(exchange, Map.of("status", "SUCCESS", "transactionId", transactionId));
-            } catch (Exception e) {
-                sendError(exchange, e.getMessage());
-            }
-        });
-
-
-        // Balance Check Endpoint (Optional)
-        server.createContext("/api/balance", exchange -> {
-            try {
-                if (!"GET".equals(exchange.getRequestMethod())) {
-                    sendError(exchange, "Only GET requests are allowed");
-                    return;
-                }
-
-                String query = exchange.getRequestURI().getQuery();
-                String bankName = getQueryParam(query, "bankName");
-
                 BankService service = manager.getOrCreateBank(bankName);
                 int balance = service.getBalance(bankName);
 
-                sendResponse(exchange, Map.of("status", "SUCCESS", "balance", balance));
+                sendResult(ctx, Map.of("status", SUCCESS, "balance", balance));
             } catch (Exception e) {
-                sendError(exchange, e.getMessage());
+                sendError(ctx, Map.of("status", ERROR, "message", e.toString()));
             }
-        });
+        }
+    }
+    
+    private class CreateBankHandler implements Handler {
+        @Override
+        public void handle(Context ctx) throws Exception {
+            logger.debug("BankController handling 'createBank' request");
 
-        server.createContext("/api/createBank", exchange -> {
             try {
-                String query = exchange.getRequestURI().getQuery();
-                String bankName = getQueryParam(query, "bankName");
-                int initialBalance = Integer.parseInt(getQueryParam(query, "initialBalance"));
+                String bankName = ctx.req().getParameter("bankName");
+                int initialBalance = Integer.parseInt(ctx.req().getParameter("initialBalance"));
 
+                BankService service = manager.getOrCreateBank(bankName);
                 manager.getOrCreateBank(bankName).createBank(bankName, initialBalance);
-                sendResponse(exchange, Map.of("status", "SUCCESS", "message", "Bank created successfully"));
+                
+                sendResult(ctx, Map.of("status", SUCCESS, "message", "Created '" + bankName + "' account"));
             } catch (Exception e) {
-                sendError(exchange, e.getMessage());
+                sendError(ctx, Map.of("status", ERROR, "message", e.toString()));
             }
-        });
+        }
+    }
+    
+    private class DeleteBankHandler implements Handler {
+        @Override
+        public void handle(Context ctx) throws Exception {
+            logger.debug("BankController handling 'deleteBank' request");
 
-        server.createContext("/api/listBanks", exchange -> {
             try {
-                if (!"GET".equals(exchange.getRequestMethod())) {
-                    sendError(exchange, "Only GET requests are allowed");
-                    return;
+                String bankName = ctx.req().getParameter("bankName");
+
+                BankService service = manager.getOrCreateBank(bankName);
+                boolean wasDeleted = service.deleteBank(bankName);
+                if (! wasDeleted) {
+                    throw new RuntimeException("Unable to delete account for '" + bankName + "'");
                 }
 
-                List<String> banks = manager.getAllBankNames();
-                sendResponse(exchange, Map.of("status", "SUCCESS", "banks", banks));
+                sendResult(ctx, Map.of("status", SUCCESS, "message", "Created '" + bankName + "' account"));
             } catch (Exception e) {
-                sendError(exchange, e.getMessage());
-            }
-        });
-
-    }
-
-    public void start() {
-        server.start();
-        System.out.println("Banking service is running on http://localhost:" + server.getAddress().getPort());
-    }
-
-    public void stop() {
-        server.stop(0);
-    }
-
-    private void sendResponse(com.sun.net.httpserver.HttpExchange exchange, Map<String, Object> response) throws IOException {
-        String jsonResponse = JsonUtil.toJson(response);
-        exchange.sendResponseHeaders(200, jsonResponse.length());
-        try (OutputStream os = exchange.getResponseBody()) {
-            os.write(jsonResponse.getBytes());
-        }
-    }
-
-    private void sendError(com.sun.net.httpserver.HttpExchange exchange, String errorMessage) throws IOException {
-        String jsonResponse = JsonUtil.toJson(Map.of("status", "ERROR", "message", errorMessage));
-        exchange.sendResponseHeaders(400, jsonResponse.length());
-        try (OutputStream os = exchange.getResponseBody()) {
-            os.write(jsonResponse.getBytes());
-        }
-    }
-
-    private String getQueryParam(String query, String paramName) {
-        for (String param : query.split("&")) {
-            String[] keyValue = param.split("=");
-            if (keyValue[0].equals(paramName)) {
-                return keyValue[1];
+                sendError(ctx, Map.of("status", ERROR, "message", e.toString()));
             }
         }
-        throw new IllegalArgumentException("Missing query parameter: " + paramName);
     }
+    
+    private class DepositHandler implements Handler {
+        @Override
+        public void handle(Context ctx) throws Exception {
+            logger.debug("BankController handling 'deposit' request");
 
-    private RequestDetails parseRequest(com.sun.net.httpserver.HttpExchange exchange) throws IOException {
-        if (!"GET".equals(exchange.getRequestMethod())) {
-            sendError(exchange, "Only GET requests are allowed");
-            return null;
-        }
+            try {
+                String bankName = ctx.req().getParameter("bankName");
+                int amount = Integer.parseInt(ctx.req().getParameter("amount"));
+                String idempotencyKey = ctx.req().getParameter("idempotencyKey");
 
-        String query = exchange.getRequestURI().getQuery();
-        String bankName = getQueryParam(query, "bankName");
-        int amount = Integer.parseInt(getQueryParam(query, "amount"));
-        String idempotencyKey = getQueryParam(query, "idempotencyKey");
-
-        BankService service = manager.getOrCreateBank(bankName);
-
-        return new RequestDetails(bankName, amount, idempotencyKey, service);
-    }
-
-    private static class RequestDetails {
-        String bankName;
-        int amount;
-        String idempotencyKey;
-        BankService service;
-
-        public RequestDetails(String bankName, int amount, String idempotencyKey, BankService service) {
-            this.bankName = bankName;
-            this.amount = amount;
-            this.idempotencyKey = idempotencyKey;
-            this.service = service;
+                BankService service = manager.getOrCreateBank(bankName);
+                String transactionId = service.deposit(bankName, amount, idempotencyKey);
+                
+                sendResult(ctx, Map.of("status", SUCCESS, "transactionId", transactionId));
+            } catch (Exception e) {
+                sendError(ctx, Map.of("status", ERROR, "message", e.toString()));
+            }
         }
     }
+    
+    private class WithdrawHandler implements Handler {
+        @Override
+        public void handle(Context ctx) throws Exception {
+            logger.debug("BankController handling 'widthdraw' request");
 
+            try {
+                String bankName = ctx.req().getParameter("bankName");
+                int amount = Integer.parseInt(ctx.req().getParameter("amount"));
+                String idempotencyKey = ctx.req().getParameter("idempotencyKey");
 
+                BankService service = manager.getOrCreateBank(bankName);
+                String transactionId = service.withdraw(bankName, amount, idempotencyKey);
+                
+                sendResult(ctx, Map.of("status", SUCCESS, "transactionId", transactionId));
+            } catch (Exception e) {
+                sendError(ctx, Map.of("status", ERROR, "message", e.toString()));
+            }
+        }
+    }
+    
+    private class GetAvailabilityHandler implements Handler {
+        @Override
+        public void handle(Context ctx) throws Exception {
+            logger.debug("BankController handling 'isAvailable' request");
+            try {
+                String bankName = ctx.req().getParameter("bankName");
+
+                BankService service = manager.getOrCreateBank(bankName);
+                boolean isAvailable = service.isAvailable(bankName);
+                sendResult(ctx, Map.of("status", SUCCESS, "available", isAvailable));
+            } catch (Exception e) {
+                sendError(ctx, Map.of("status", ERROR, "message", e.toString()));
+            }
+        }
+    }
+    
+    private class SetAvailabilityHandler implements Handler {
+        @Override
+        public void handle(Context ctx) throws Exception {
+            logger.debug("BankController handling 'setAvailable' request");
+
+            try {
+                String bankName = ctx.req().getParameter("bankName");
+                boolean wantsAvailable = Boolean.parseBoolean(ctx.req().getParameter("value"));
+
+                BankService service = manager.getOrCreateBank(bankName);
+                if (wantsAvailable) {
+                    service.startBank(bankName);
+                } else {
+                    service.stopBank(bankName);
+                }
+                
+                sendResult(ctx, Map.of("status", SUCCESS, "available", wantsAvailable));
+            } catch (Exception e) {
+                sendError(ctx, Map.of("status", ERROR, "message", e.toString()));
+            }
+        }
+    }
+    
+    private class ListBanksHandler implements Handler {
+        @Override
+        public void handle(Context ctx) throws Exception {
+            logger.debug("BankController handling 'listBanks' request");
+
+            try {
+                List<String> banks = manager.getAllBankNames();
+                sendResult(ctx, Map.of("status", SUCCESS, "banks", banks));
+            } catch (Exception e) {
+                sendError(ctx, Map.of("status", ERROR, "message", e.toString()));
+            }
+        }
+    }
+    
+    private void sendResult(Context ctx, Map data) throws JsonProcessingException {
+        logger.debug("BankController sending result");
+
+        String result = new ObjectMapper().writeValueAsString(data);
+        ctx.status(200);
+        ctx.result(result);        
+    }
+
+    private void sendError(Context ctx, Map data) throws JsonProcessingException {
+        logger.debug("BankController sending error");
+
+        String result = new ObjectMapper().writeValueAsString(data);
+        ctx.status(500);
+        ctx.result(result);        
+    }
 }
