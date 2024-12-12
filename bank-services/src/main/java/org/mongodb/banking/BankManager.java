035dccf507df366164531d0ee659212d567512f5
Tom Wheeler <tom@temporal.io>
2024-12-11 21:13:40 -0600

    added some log statements

--- bank-services/src/main/java/org/mongodb/banking/BankManager.java
+++ bank-services/src/main/java/org/mongodb/banking/BankManager.java
@@ -5,12 +5,19 @@ import org.mongodb.banking.repository.BankRepository;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BankManager {
+
+    private static final Logger logger = LoggerFactory.getLogger(BankManager.class);
+
     private final Map<String, BankService> banks = new HashMap<>();
     private final BankRepository repository;
 
     public BankManager(BankRepository repository) {
+        logger.debug("Creating BankManager instance");
+
         this.repository = repository;
     }
 
