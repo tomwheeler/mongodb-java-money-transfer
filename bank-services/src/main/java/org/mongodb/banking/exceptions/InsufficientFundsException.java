43b1b41db59685781d5719c34de231cf689fdae9
Tom Wheeler <tom@temporal.io>
2024-12-11 21:13:20 -0600

    added javadoc for class

--- bank-services/src/main/java/org/mongodb/banking/exceptions/InsufficientFundsException.java
+++ bank-services/src/main/java/org/mongodb/banking/exceptions/InsufficientFundsException.java
@@ -1,5 +1,9 @@
 package org.mongodb.banking.exceptions;
 
+/**
+ * Exception thrown when the amount requested in the withdrawal exceeds
+ * the balance of the source account.
+ */
 public class InsufficientFundsException extends RuntimeException {
     public InsufficientFundsException(String message) {
         super(message);
