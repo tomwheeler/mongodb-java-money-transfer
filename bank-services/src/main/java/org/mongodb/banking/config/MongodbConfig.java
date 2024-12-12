d1d78f9019b668be350806d502cad243c7b1c237
Tom Wheeler <tom@temporal.io>
2024-12-11 21:17:21 -0600

    made name of environment variable a public constant so that I could check in the Main class whether it's defined

--- bank-services/src/main/java/org/mongodb/banking/config/MongodbConfig.java
+++ bank-services/src/main/java/org/mongodb/banking/config/MongodbConfig.java
@@ -5,20 +5,27 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
 public class MongodbConfig {
-    private static final String CONNECTION_STRING = System.getenv("MONGO_CONNECTION_STRING");
-    private static final String DATABASE_NAME = "banking";
+
+    public static final String CONN_STRING_ENV_VARNAME = "MONGO_CONNECTION_STRING";
+    
+    private static final String CONNECTION_STRING = System.getenv(CONN_STRING_ENV_VARNAME);
+    private static final String DATABASE_NAME = "bankingdemo";
 
     /**
-     * Returns the database with the default name, accessible through a connection
-     * string defined in the <code>MONGO_CONNECTION_STRING</code> environment variable.
+     * Returns the database with the default name, accessible through a
+     * connection string defined through an environment variable.
      */
     public static MongoDatabase getDatabase() {
         return getDatabase(DATABASE_NAME, CONNECTION_STRING);
     }
 
     /**
-     * Returns the database with the specified name, accessible through the specified
-     * connection string.
+     * Returns the database with the specified name, accessible through the
+     * specified connection string.
+     * 
+     * @param databaseName the name of the database to connect to
+     * @param connectionString specifies details for connecting to that database
+     * @return the MongoDatabase corresponding to the input parameters
      */
     public static MongoDatabase getDatabase(String databaseName, String connectionString) {
         MongoClient client = MongoClients.create(connectionString);
