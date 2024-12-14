07d5451e3593fc8eb2aac53d99d5414f5d2456f3
Tom Wheeler <tom@temporal.io>
2024-12-14 17:06:07 -0600

    removed unused code and improved the Javadoc

--- money-transfer-service/src/main/java/org/mongodb/exceptions/AccountOperationException.java
+++ money-transfer-service/src/main/java/org/mongodb/exceptions/AccountOperationException.java
@@ -1,27 +1,12 @@
 package org.mongodb.exceptions;
 
 /**
- * Represents a business-level failure encountered during an operation on
- * a bank account. For example, the application will throw this error during
- * an attempt to withdraw an amount beyond the account balance. This is in
- * contrast to, for example, the IOException that's thrown if the application
- * cannot connect to the banking service at all.
+ * Represents a business-level failure encountered by the client during
+ * an operation on a bank account. This type of exception is used for
+ * the general case, while its subclasses are used for specific ones.
  */
-public class AccountOperationException extends Exception {
-
-    public AccountOperationException() {
-        super();
-    }
-
+public class AccountOperationException extends RuntimeException {
     public AccountOperationException(String message) {
         super(message);
-    }
-
-    public AccountOperationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public AccountOperationException(Throwable cause) {
-        super(cause);
     }
 }
