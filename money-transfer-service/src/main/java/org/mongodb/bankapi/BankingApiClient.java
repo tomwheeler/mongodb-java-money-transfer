10e4d7583f8f0e70338bcc0fa6de5e6846fd1a55
Tom Wheeler <tom@temporal.io>
2024-12-14 17:06:38 -0600

    simplified and improved testability for this code by moving JSON parsing to a separate class

--- money-transfer-service/src/main/java/org/mongodb/bankapi/BankingApiClient.java
+++ money-transfer-service/src/main/java/org/mongodb/bankapi/BankingApiClient.java
@@ -5,8 +5,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.net.URLEncoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,10 +17,9 @@ public class BankingApiClient {
 
     private static final Logger logger = LoggerFactory.getLogger(BankingApiClient.class);
 
-    private static final String SUCCESS = "SUCCESS"; // service returns this to indicate success
-
     private final String hostname;
     private final int portNumber;
+    private final MessageParser parser;
 
     /**
      * Creates a new instance that will access services on the specified
@@ -34,29 +31,24 @@ public class BankingApiClient {
     public BankingApiClient(String hostname, int portNumber) {
         this.hostname = hostname;
         this.portNumber = portNumber;
+        parser = new MessageParser();
     }
 
     /**
-     * Return the current balance for the specified bank account.
+     * Return the current balance for the specified bank account. In addition to the IOException,
+     * it may throw NoSuchAccountException (if the account is unknown) or AccountOperationException,
+     * for other types of application-level failures.
+     *
      * @param bankName identifies the account for which the balance is being requested
+     * @throws IOException if it encounters failure while making the call or parsing the response
      * @return the current balance of the specified account.
      */
-    public int getBalance(String bankName) throws Exception {
+    public int getBalance(String bankName) throws IOException {
         String name = URLEncoder.encode(bankName, "UTF-8");
         String url = String.format("http://%s:%d/api/balance?bankName=%s", hostname, portNumber, name);
 
         String body = callService(url);
-
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode bodyNode = mapper.readTree(body);
-        JsonNode statusNode = bodyNode.get("status");
-        if (! SUCCESS.equals(statusNode.asText())) {
-            String message = bodyNode.get("message").asText();
-            throw new IOException("balance retrieval call unsuccessful, message is: " + message);
-        }
-
-        JsonNode balanceNode = bodyNode.get("balance");
-        int balance = balanceNode.asInt();
+        int balance = parser.parseBalanceResponse(body);
 
         return balance;
     }
@@ -65,38 +57,35 @@ public class BankingApiClient {
      * Requests a deposit of the specified amount to the named account. The idempotency key is
      * used to identify duplicate requests. If this key matches one previously seen in this
      * session, this method will not perform the deposit. Instead, it will return the transaction
-     * ID for that earlier transaction.
+     * ID for that earlier transaction. In addition to the IOException, it may throw
+     * NoSuchAccountException (if the account is unknown) or AccountOperationException (for
+     * other types of application-level failures).
      *
      * @param bankName the account to which the money should be credited
      * @param amount the quantity by which the account balance should be increased
      * @param idempotencyKey a user-specified unique key for this request
      * @return the transaction ID for the deposit
-     * @throws Exception if a problem is encountered during the request
+     * @throws IOException if it encounters failure while making the call or parsing the response
      */
-    public String deposit(String bankName, int amount, String idempotencyKey) throws Exception {
+    public String deposit(String bankName, int amount, String idempotencyKey) throws IOException {
         String name = URLEncoder.encode(bankName, "UTF-8");
         String key = URLEncoder.encode(idempotencyKey, "UTF-8");
         String baseUrl = "http://%s:%d/api/deposit?bankName=%s&amount=%d&idempotencyKey=%s";
         String url = String.format(baseUrl, hostname, portNumber, name, amount, key);
 
         String body = callService(url);
+        String transactionId = parser.parseDepositResponse(body);
 
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode bodyNode = mapper.readTree(body);
-        JsonNode statusNode = bodyNode.get("status");
-        if (! SUCCESS.equals(statusNode.asText())) {
-            String message = bodyNode.get("message").asText();
-            throw new IOException("balance retrieval call unsuccessful, message is: " + message);
-        }
-
-        return bodyNode.get("transactionId").asText();
+        return transactionId;
     }
 
     /**
      * Requests a withdrawal of the specified amount from the named account. The idempotency key is
-     * used to identify duplicate requests. If this key matches one previously seen in this
-     * session, this method will not perform the withdrawal. Instead, it will return the transaction
-     * ID for that earlier transaction.
+     * used to identify duplicate requests. If this key matches one previously seen in this session,
+     * this method will not perform the withdrawal. Instead, it will return the transaction ID for
+     * that earlier transaction. In addition to IOException, it may throw InsufficientFundsException
+     * (if the amount exceeds the account balance), NoSuchAccountException (if the account is unknown)
+     * or AccountOperationException (for other types of application-level failures)
      *
      * @param bankName the account from which the money should be debited
      * @param amount the quantity by which the account balance should be decreased
@@ -104,23 +93,16 @@ public class BankingApiClient {
      * @return the transaction ID for the withdrawal
      * @throws Exception if a problem is encountered during the request
      */
-    public String withdraw(String bankName,  int amount, String idempotencyKey) throws Exception {
+    public String withdraw(String bankName, int amount, String idempotencyKey) throws Exception {
         String name = URLEncoder.encode(bankName, "UTF-8");
         String key = URLEncoder.encode(idempotencyKey, "UTF-8");
         String baseUrl = "http://%s:%d/api/withdraw?bankName=%s&amount=%d&idempotencyKey=%s";
         String url = String.format(baseUrl, hostname, portNumber, name, amount, key);
 
         String body = callService(url);
+        String transactionId = parser.parseWithdrawResponse(body);
 
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode bodyNode = mapper.readTree(body);
-        JsonNode statusNode = bodyNode.get("status");
-        if (! SUCCESS.equals(statusNode.asText())) {
-            String message = bodyNode.get("message").asText();
-            throw new IOException("balance retrieval call unsuccessful, message is: " + message);
-        }
-
-        return bodyNode.get("transactionId").asText();
+        return transactionId;
     }
 
     private String callService(String serviceUrl) throws IOException {
